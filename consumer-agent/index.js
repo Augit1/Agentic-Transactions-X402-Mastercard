63902a7248acefc11e2ec4e711f74c25ed78933c
Augustin-MacBook <augustinbethery007@gmail.com>
2025-11-29 12:55:59 +0100

    fixed CORS error

--- consumer-agent/index.js
+++ consumer-agent/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const path = require("path");
 const routes = require("./routes");
 
 const app = express();
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "..", "ui")));
+
 app.use("/", routes);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "ui", "index.html"));
+})
 
 const PORT = 4001;
 app.listen(PORT, () => console.log(`Consumer Agent running on port ${PORT}`));
